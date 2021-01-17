0b1f19c69c965b70235d4c37a73a4d7c2652a383
takahashi <takahashi@goodbee.jp>
2021-01-17 18:53:23 +0900

    forceHttps

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 強制的に、HTTPS
+        if (\App::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
