a668fa0ca642abb7a9906d74876fa1d0eb5efa3d
takahashi <takahashi@goodbee.jp>
2021-01-21 22:45:06 +0900

    本番環境でdevtoolのメッセージを表示しないように設定

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import Top from './Top';
 
 const App = () => {
+  // コンソール上のdevtoolに関するメッセージを本番環境では表示しないようにする。
+  if (process.env.NODE_ENV === 'production') {
+    window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {}
+  }
 
   return(
     <Top/>
