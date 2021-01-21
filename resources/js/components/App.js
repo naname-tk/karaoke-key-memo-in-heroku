63638405dcccb26681d9a9d999e27d45a90e2587
takahashi <takahashi@goodbee.jp>
2021-01-21 23:31:47 +0900

    本番環境でdevtoolに関するメッセージを出さないようにblade上で設定

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import Top from './Top';
 
 const App = () => {
-  // コンソール上のdevtoolに関するメッセージを本番環境では表示しないようにする。
-  if (process.env.NODE_ENV === 'production') {
-    window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {}
-  }
-  console.log(process.env.NODE_ENV);
-
   return(
     <Top/>
   )
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,20 +1,29 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'React導入テスト') }}</title>
-    <script src="{!! myAsset('js/app.js') !!}" defer></script>
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    @include('common.favicon')
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="{!! myAsset('css/app.css') !!}" rel="stylesheet">
-</head>
-<body>
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+      <title>{{ config('app.name', 'React導入テスト') }}</title>
+      <script src="{!! myAsset('js/app.js') !!}" defer></script>
+      <link rel="dns-prefetch" href="//fonts.gstatic.com">
+      @include('common.favicon')
+      <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+      <link href="{!! myAsset('css/app.css') !!}" rel="stylesheet">
+  </head>
+  <body>
     <script>
-        window.const = {!! $const !!}
+      // 本番環境上でコンソール上のdevtoolに関するメッセージを本番環境では表示しないようにする。
+      @production
+        window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {
+          supportsFiber: true,
+          inject: function() {},
+          onCommitFiberRoot: function() {},
+          onCommitFiberUnmount: function() {},
+        };
+      @endproduction
+      window.const = {!! $const !!}
     </script>
-<div id="app"></div>
-</body>
+    <div id="app"></div>
+  </body>
 </html>
